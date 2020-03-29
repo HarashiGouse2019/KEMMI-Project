8827343f246d56fe69aeba5f29dd63d5a9a7cd32
Tyrease Teer <kayaneseera@gmail.com>
2020-03-28 21:12:48 -0700

    Make Faces a bit better; added mouse interaction
    You can click on things with the MouseDataReceiver

---
+++ Assets/Scripts/MouseControls.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControls : MonoBehaviour
+{
+    static MouseControls Instance;
+
+    //Mouse controls
+
+    public static string CurrentlyOn { get; set; } = "";
+    public static GameObject Object { get; set; } = null;
+
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Object.transform.position = Input.mousePosition;
+    }
+
+    public static GameObject SetCurrentlyOn(GameObject _obj)
+    {
+        CurrentlyOn = _obj.name;
+        Object = _obj;
+        return Object;
+    }
+}
---
+++ Assets/Scripts/MouseDataReceiver.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class MouseDataReceiver : MonoBehaviour
+{
+    //Something to put on a gameObject
+    GameObject entityObj;
+    Vector3 screenPoint;
+    Vector3 offset;
+
+    [Header("Draggable")]
+    public bool dragable = false;
+
+    void Awake()
+    {
+        entityObj = gameObject;
+    }
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.ViewportToScreenPoint(Input.mousePosition);
+
+        offset = entityObj.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    }
+
+    void OnMouseDrag()
+    {
+        if (dragable) {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+
+            entityObj.transform.position = curPosition;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        entityObj = null;
+    }
+}
